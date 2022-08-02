f92a155dafa597a3c7975fd7596bc8874978d6a0
jidabao <110473691+jidabao@users.noreply.github.com>
2022-08-02 22:47:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "点我",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
